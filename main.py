905680d84cc0e59d184315d343a9236fb7f505f1
Ivan Ponomarev <ponomarev777@list.ru>
2024-07-14 10:24:29 +0300

    SECOND WEAK SOLUTION. Rollback when flet will be updated. Trying to connect to the Vercel db

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from fastapi.staticfiles import StaticFiles
 from funcs import clean_temp_dir, create_temp_folder
 from config import dokapp_temp
 from router import router as main_router
+from queries import add_object_to_db
+
+from models import Worker
+from database import Base, engine
 
 
 # Создаем временную папку, если ее нет
@@ -18,3 +22,8 @@ app.mount(f'/{dokapp_temp}', StaticFiles(directory=f'{dokapp_temp}'), name=f'{do
 clean_temp_dir(dokapp_temp)
 
 app.include_router(main_router)
+
+Base.metadata.create_all(engine)
+
+new_worker = Worker(name="Ivan")
+add_object_to_db(new_worker)
