7d9e0b317e3b3057d152554ebce63a8da6341cf2
Ivan <ponomarev777@list.ru>
2024-07-22 18:05:35 +0300

    correct naming of the photo because of additional list of received names

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-import os
+# import os
 
-dokapp_temp = f"{os.path.dirname(os.path.abspath(__file__))}/tmp"
+# dokapp_temp = f"{os.path.dirname(os.path.abspath(__file__))}/tmp"
+dokapp_temp = "/tmp"
 photo_title_lenght = 80
